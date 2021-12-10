27939c4e4bd709c07e7a3938fbbbd10fd0b135eb
PeterGhimself <petergranitski@gmail.com>
2021-12-09 20:53:07 -0500

    Refactor to handle 4 more pre-trained models

--- task1.py
+++ task1.py
@@ -17,7 +17,6 @@ import gensim.downloader as api
 
 # logging setup
 
-
 def setup_logger(logger_name, log_file, level=logging.INFO):
 
     log_setup = logging.getLogger(logger_name)
@@ -42,144 +41,155 @@ def logger(msg, logfile):
     
     log.info(msg) 
 
-GOOGLE_NEWS_MODEL = 'word2vec-google-news-300'
-SYNONYMS_CSV_FILE = './synonyms.csv'
-GOOGLE_NEWS_DETAILS_FILE = GOOGLE_NEWS_MODEL + '-details.txt'
-GOOGLE_NEWS_ANALYSIS_FILE = GOOGLE_NEWS_MODEL + '-analysis.txt'
-TEST_DATA_SIZE = 80
 
-setup_logger('details', GOOGLE_NEWS_DETAILS_FILE, logging.INFO)
-setup_logger('analysis', GOOGLE_NEWS_ANALYSIS_FILE, logging.INFO)
+def run_model(model_name):
+    SYNONYMS_CSV_FILE = './synonyms.csv'
+    DETAILS_FILE = model_name + '-details.txt'
+    ANALYSIS_FILE = model_name + '-analysis.txt'
+    TEST_DATA_SIZE = 80
 
-# ensure log files reset per run
-with open(GOOGLE_NEWS_DETAILS_FILE, 'w') as fout:
-    fout.writelines('')
-with open(GOOGLE_NEWS_ANALYSIS_FILE, 'w') as fout:
-    fout.writelines('')
+    setup_logger('details', DETAILS_FILE, logging.INFO)
+    setup_logger('analysis', ANALYSIS_FILE, logging.INFO)
 
-print('Loading: ' + GOOGLE_NEWS_MODEL)
+    # ensure log files reset per run
+    with open(DETAILS_FILE, 'w') as fout:
+        fout.writelines('')
+    with open(ANALYSIS_FILE, 'w') as fout:
+        fout.writelines('')
 
-start = time.time()
-model = api.load(GOOGLE_NEWS_MODEL)
-word_vectors = model.index_to_key # used later for checking if certain words included or not
-model_load_time = time.time() - start
+    print('Loading: ' + model_name)
 
-print('\nTook ' + str(model_load_time) + ' seconds to load GoogleNews embedding model')
+    start = time.time()
+    model = api.load(model_name)
+    word_vectors = model.index_to_key # used later for checking if certain words included or not
+    model_load_time = time.time() - start
 
-print('\nSimilarity of "woman" and "man" --> ' + str(model.similarity('woman', 'man')))
+    print('\nTook ' + str(model_load_time) + ' seconds to load ' + model_name + 'embedding model')
 
-'''
-The output of this task should be stored in 2 files:
-1. In a file called <model name>-details.csv, for each question in the Synonym Test dataset, in a single line:
-(a) the question-word, a comma,
-(b) the correct answer-word, a comma
-(c) your system’s guess-word, a comma
-(d) one of 3 possible labels:
-• the label guess, if either question-word or all four guess-words (or all 5 words) were not found in
-the embedding model (so if the question-word was present in the model, and at least 1 guess-word
-was present also, you should not use this label).
-• the label correct, if the question-word and at least 1 guess-word were present in the model, and
-the guess-word was correct.
-• the label wrong if the question-word and at least 1 guess-word were present in the model, and the
-guess-word was not correct.
-For example, the file word2vec-google-news-300-details.csv could contain:
-enormously,tremendously,uniquely,wrong
-provisions,stipulations,stipulations,correct
-...
-2. In a file called analysis.csv, in a single line:
-(a) the model name (clearly indicating the source of the corpus and the vector size), a comma
-(b) the size of the vocabulary (the number of unique words in the corpus1
-)
-(c) the number of correct labels (call this C), a comma
-(d) the number of questions that your model answered without guessing (i.e. 80− guess) (call this V ), a
-comma
-(e) the accuracy of the model (i.e. C
-V
-)
-For example, the file analysis.csv could contain:
-word2vec-google-news-300,3000000,44,78,0.5641025641025641
+    '''
+    The output of this task should be stored in 2 files:
+    1. In a file called <model name>-details.csv, for each question in the Synonym Test dataset, in a single line:
+    (a) the question-word, a comma,
+    (b) the correct answer-word, a comma
+    (c) your system’s guess-word, a comma
+    (d) one of 3 possible labels:
+    • the label guess, if either question-word or all four guess-words (or all 5 words) were not found in
+    the embedding model (so if the question-word was present in the model, and at least 1 guess-word
+    was present also, you should not use this label).
+    • the label correct, if the question-word and at least 1 guess-word were present in the model, and
+    the guess-word was correct.
+    • the label wrong if the question-word and at least 1 guess-word were present in the model, and the
+    guess-word was not correct.
+    For example, the file word2vec-google-news-300-details.csv could contain:
+    enormously,tremendously,uniquely,wrong
+    provisions,stipulations,stipulations,correct
+    ...
+    2. In a file called analysis.csv, in a single line:
+    (a) the model name (clearly indicating the source of the corpus and the vector size), a comma
+    (b) the size of the vocabulary (the number of unique words in the corpus1
+    )
+    (c) the number of correct labels (call this C), a comma
+    (d) the number of questions that your model answered without guessing (i.e. 80− guess) (call this V ), a
+    comma
+    (e) the accuracy of the model (i.e. C
+    V
+    )
+    For example, the file analysis.csv could contain:
+    word2vec-google-news-300,3000000,44,78,0.5641025641025641
 
-'''
+    '''
 
-with open(SYNONYMS_CSV_FILE) as f:
-    lines = f.readlines()
+    with open(SYNONYMS_CSV_FILE) as f:
+        lines = f.readlines()
 
-lines = lines[1:] # strip away first header line
-log_line = ''
-correct_ctr = guess_ctr = 0
+    lines = lines[1:] # strip away first header line
+    log_line = ''
+    correct_ctr = guess_ctr = 0
 
-for line in lines:
-    print('----------------------')
-    line = line.strip()
-    tings = line.split(',')
-    question_word = tings[0]
-    question_answer = tings[1]
-    options = tings[2:]
+    for line in lines:
+        print('----------------------')
+        line = line.strip()
+        tings = line.split(',')
+        question_word = tings[0]
+        question_answer = tings[1]
+        options = tings[2:]
 
-    print('question word:', question_word)
-    print('question answer:', question_answer)
-    print('guess options:', options)
+        print('question word:', question_word)
+        print('question answer:', question_answer)
+        print('guess options:', options)
 
-    log_line = question_word + ',' +  question_answer + ','
-    best_guess = label = ''
-    max_similarity = 0 
+        log_line = question_word + ',' +  question_answer + ','
+        best_guess = label = ''
+        max_similarity = 0 
 
-    for guess in options:
-        try:
-            print('comparing "' + question_word + '" with "' + guess + '"')
-            similarity = model.similarity(question_word, guess)
-            print('similarity:', similarity)
-            print('max_similarity:', max_similarity)
+        for guess in options:
+            try:
+                print('comparing "' + question_word + '" with "' + guess + '"')
+                similarity = model.similarity(question_word, guess)
+                print('similarity:', similarity)
+                print('max_similarity:', max_similarity)
 
-            if similarity > max_similarity:
-                max_similarity = similarity
-                best_guess = guess
+                if similarity > max_similarity:
+                    max_similarity = similarity
+                    best_guess = guess
 
-        except KeyError:
-            if question_word not in word_vectors:
-                print('question word "' + question_word + '" not found in model')
-                label = 'guess'
-            else:
-                at_least_one_found = False
-                for guess in options:
-                    if guess not in word_vectors:
-                        print('guess "' + guess + '" not found in model')
-                    else:
-                        at_least_one_found = True
-
-                if not at_least_one_found:
+            except KeyError:
+                if question_word not in word_vectors:
+                    print('question word "' + question_word + '" not found in model')
                     label = 'guess'
-        finally:
-            if not label == 'guess':
-                if best_guess == question_answer:
-                    label = 'correct'
                 else:
-                    label = 'wrong'
+                    at_least_one_found = False
+                    for guess in options:
+                        if guess not in word_vectors:
+                            print('guess "' + guess + '" not found in model')
+                        else:
+                            at_least_one_found = True
+
+                    if not at_least_one_found:
+                        label = 'guess'
+            finally:
+                if not label == 'guess':
+                    if best_guess == question_answer:
+                        label = 'correct'
+                    else:
+                        label = 'wrong'
 
 
-    if label == 'guess':
-        guess_ctr += 1
-    elif label == 'correct':
-        correct_ctr += 1
+        if label == 'guess':
+            guess_ctr += 1
+        elif label == 'correct':
+            correct_ctr += 1
 
-    print('best guess:', best_guess)
-    print('label:', label)
+        print('best guess:', best_guess)
+        print('label:', label)
 
-    log_line += best_guess + ',' + label
-    logger(log_line, 0)
+        log_line += best_guess + ',' + label
+        logger(log_line, 0)
 
 
-# stats needed for analysis file
-vocab_size = len(word_vectors)
-num_questions_not_guessed = TEST_DATA_SIZE - guess_ctr
-model_accuracy = (correct_ctr/num_questions_not_guessed) * 100
-model_accuracy = round(model_accuracy, 2)
+    # stats needed for analysis file
+    vocab_size = len(word_vectors)
+    num_questions_not_guessed = TEST_DATA_SIZE - guess_ctr
+    model_accuracy = (correct_ctr/num_questions_not_guessed) * 100
+    model_accuracy = round(model_accuracy, 2)
 
-print('guess_ctr', guess_ctr)
-print('correct_ctr', correct_ctr)
-print('num_questions_not_guessed)', num_questions_not_guessed)
+    print('guess_ctr', guess_ctr)
+    print('correct_ctr', correct_ctr)
+    print('num_questions_not_guessed)', num_questions_not_guessed)
 
-log_line = GOOGLE_NEWS_MODEL + ',' + str(vocab_size) + ',' + str(correct_ctr) + ','
-log_line += str(num_questions_not_guessed) + ',' + str(model_accuracy) + '%'
+    log_line = model_name + ',' + str(vocab_size) + ',' + str(correct_ctr) + ','
+    log_line += str(num_questions_not_guessed) + ',' + str(model_accuracy) + '%'
 
-logger(log_line, 1)
+    logger(log_line, 1)
+
+#run_model('word2vec-google-news-300') # OG (variant 0)
+
+# different corpus, same embedding sizes
+#run_model('glove-wiki-gigaword-300') # variant 1
+run_model('conceptnet-numberbatch-17-06-300')
+#run_model('fasttext-wiki-news-subwords-300') # takes forever to process? not done loading? what is happening?
+
+# same corpus, different embedding sizes
+run_model('glove-twitter-25')
+run_model('glove-twitter-200')
+
